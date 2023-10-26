fce1a3ba52b0de379c4c56c9a8495de66cb342cc
Nakul A <matrixlukan@gmail.com>
2023-10-26 15:51:12 +0530

    Search devices

--- JailTalk/JailTalk.Application/Requests/Device/GetAllDevicesRequest.cs
+++ JailTalk/JailTalk.Application/Requests/Device/GetAllDevicesRequest.cs
@@ -8,10 +8,13 @@ using Microsoft.EntityFrameworkCore;
 
 namespace JailTalk.Application.Requests.Device;
 
-public class GetAllDevicesRequest : IRequest<ResponseDto<List<DeviceListDto>>>
+public class GetAllDevicesRequest : IRequest<PaginatedResponse<DeviceListDto>>
 {
+    public string SearchValue { get; set; }
+    public int? Skip { get; set; }
+    public int? Take { get; set; }
 }
-public class GetAllDevicesRequestHandler : IRequestHandler<GetAllDevicesRequest, ResponseDto<List<DeviceListDto>>>
+public class GetAllDevicesRequestHandler : IRequestHandler<GetAllDevicesRequest, PaginatedResponse<DeviceListDto>>
 {
     readonly IAppDbContext _appDbContext;
     readonly IAppRequestContext _requestContext;
@@ -22,25 +25,31 @@ public class GetAllDevicesRequestHandler : IRequestHandler<GetAllDevicesRequest,
         _requestContext = requestContext;
     }
 
-    public async Task<ResponseDto<List<DeviceListDto>>> Handle(GetAllDevicesRequest request, CancellationToken cancellationToken)
+    public async Task<PaginatedResponse<DeviceListDto>> Handle(GetAllDevicesRequest request, CancellationToken cancellationToken)
     {
         var prisonId = _requestContext.GetAssociatedPrisonId();
-        var devices = await _appDbContext.Devices
+        var devicesQuery = _appDbContext.Devices
             .WhereInPrison(x => x.JailId, prisonId)
-            .OrderBy(x => x.Code)
+            .Where(x => string.IsNullOrEmpty(request.SearchValue) || (x.Code.Contains(request.SearchValue) || x.MacAddress.Contains(request.SearchValue)))
+            .OrderBy(x => x.Code);
+
+        var totalCount = await devicesQuery.CountAsync(cancellationToken);
+        var devices = await devicesQuery
             .Include(x => x.Jail)
+            .Skip(request.Skip.Value)
+            .Take(request.Take.Value)
             .ToListAsync(cancellationToken);
         int index = 1;
-        return new(devices.Select(x => new DeviceListDto
+        return new PaginatedResponse<DeviceListDto>(devices.Select(x => new DeviceListDto
         {
             DeviceId = x.Id,
             Code = x.Code,
-            DeviceSecret = x.DeviceSecretIdentifier.ToString().Mask(4, 4),
+            MacAddress = x.MacAddress.ToString().Mask(3, 3),
             IsActive = x.IsActive,
             Prison = $"{x.Jail.Code} ({x.Jail.Name})",
             IsDisabledAtJailLevel = x.Jail.IsSystemTurnedOff,
             Serial = index++,
-        }).ToList());
+        }).ToList(), totalCount);
     }
 }
 
